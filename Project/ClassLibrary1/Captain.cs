79d6cdfeaabf9cdeb55c92da42baf566506b44a2
norleaf <norleaf@gmail.com>
2019-01-21 07:52:46 +0100

    Captain Cards
    todo: still fix cards

---
+++ Project/ClassLibrary1/Captain.cs
@@ -0,0 +1,24 @@
+﻿using BoardGraph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Captain
+    {
+
+
+        public Queue<Card> CaptainCards()
+        {
+            Queue<Card> cards = new Queue<Card>();
+            for (int i = 0; i < 10; i++)
+            {
+                cards.Enqueue(new Card("default", new Option {name = "do nothing"}));
+            }
+        }
+
+    }
+}
