e6af21fe71affbaa045ac948c7e2bd853ef10a45
norleaf <norleaf@gmail.com>
2019-02-08 14:44:54 +0100

    and more

---
+++ Project/BoardGraph/EventCard.cs
@@ -0,0 +1,18 @@
+﻿namespace BoardGraph
+{
+    public abstract class EventCard
+    {
+        public string name;
+        public string description;
+        public string[] types;
+
+
+
+        public void MoveEnemies(Board board)
+        {
+
+        }
+
+        public abstract void ResolveEvent(Board board);
+    }
+}
